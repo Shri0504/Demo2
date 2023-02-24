387cc042198bceca25a8f5ceb838cdd46069c585
Shri0504 <patilshrikant00504@gmail.com>
2023-02-24 13:18:25 +0530

    my phonetracker program

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hii bhaii")
+import phonenumbers
+from phonenumbers import carrier, geocoder, timezone
+
+mobileNo = input("Enter mobile number with country code: ")
+mobileNo = phonenumbers.parse(mobileNo)
+print(timezone.time_zones_for_number(mobileNo))
+print(carrier.name_for_number(mobileNo, "en"))
+print(geocoder.description_for_number(mobileNo, "en"))
+print("valid Mobile Number :", phonenumbers.is_valid_number(mobileNo))
+print("Checking possibility of number :", phonenumbers. is_possible_number(mobileNo))
